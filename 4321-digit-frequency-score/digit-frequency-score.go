fba3685e36862a603853edef499f4c402baaf736
kabir hamdullah siddiqui <kabirsiddiqui014@gmail.com>
2026-07-31 18:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (44.11%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.go
+++ 4321-digit-frequency-score/digit-frequency-score.go
@@ -1,16 +1,18 @@
 func digitFrequencyScore(n int) int {
-    var num []int
-    for n>0 {
-        num=append(num,n%10)
-        n=n/10
-    }
-    // freq := map[int]int{}
-    // for _,value := range num{
-    //     freq[value]++
+    // var num []int
+    // for n>0 {
+    //     num=append(num,n%10)
+    //     n=n/10
     // }
     score:=0
-    for _,value := range num{
-        score+=value
+    // for _,value := range num{
+    //     score+=value
+    // }
+    var digit int
+    for n>0{
+        digit=n%10
+        score+=digit
+        n=n/10
     }
     return score
 }
