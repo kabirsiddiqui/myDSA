92870168c46a85ad428855d93f2a28df9db5c9d1
kabir hamdullah siddiqui <kabirsiddiqui014@gmail.com>
2026-08-02 12:43:32 +0530

    Time: 0 ms (100.00%) | Memory: 3.9 MB (100.00%) - LeetSync

---
+++ 909-stone-game/stone-game.go
@@ -0,0 +1,16 @@
+func stoneGame(piles []int) bool {
+    // i:=0
+    // j:=len(piles)-1
+    // if score(i,j,piles)>=0{
+    //     return true
+    // }else{
+    //     return false
+    // }
+    return true
+}
+// func score(i,j int,piles []int) int {
+//     if i==j{
+//         return piles[i]
+//     }
+//     return max(piles[i]-score(i+1,j,piles),piles[j]-score(i,j-1,piles))
+// }
