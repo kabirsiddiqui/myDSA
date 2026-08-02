01b3765f62a6d9db4ea3d23f72046bdafdf416eb
kabir hamdullah siddiqui <kabirsiddiqui014@gmail.com>
2026-08-02 13:12:54 +0530

    Time: 18 ms (12.20%) | Memory: 9.2 MB (14.63%) - LeetSync

--- 909-stone-game/stone-game.go
+++ 909-stone-game/stone-game.go
@@ -1,16 +1,27 @@
 func stoneGame(piles []int) bool {
-    // i:=0
-    // j:=len(piles)-1
-    // if score(i,j,piles)>=0{
-    //     return true
-    // }else{
-    //     return false
-    // }
+    cache := make([][]int, len(piles))
+    for i := range cache {
+        cache[i] = make([]int, len(piles))
+        for j := range cache[i] {
+            cache[i][j] = math.MinInt
+        }
+    }
+    if score(0,len(piles)-1,piles,cache)>=0{
+        return true
+    }else{
+        return false
+    }
     return true
 }
-// func score(i,j int,piles []int) int {
-//     if i==j{
-//         return piles[i]
-//     }
-//     return max(piles[i]-score(i+1,j,piles),piles[j]-score(i,j-1,piles))
-// }
+func score(i,j int,piles []int,cache [][]int) int {
+    if i==j{
+        return piles[i]
+    }
+    if cache[i][j]==math.MinInt{
+        cache[i][j]=max(piles[i]-score(i+1,j,piles,cache),piles[j]-score(i,j-1,piles,cache))
+        return max(piles[i]-score(i+1,j,piles,cache),piles[j]-score(i,j-1,piles,cache))
+    }else{
+        return cache[i][j]
+    }
+    
+}
