57bdbc39e21d086777db0b9335b5176423dcae31
kabir hamdullah siddiqui <kabirsiddiqui014@gmail.com>
2026-08-14 11:01:40 +0530

    Time: 0 ms (100.00%) | Memory: 4.2 MB (11.76%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.go
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.go
@@ -1,16 +1,16 @@
 func maximumLengthSubstring(s string) int {
-    freq:=map[string]int{}
+    freq:=map[byte]int{}
     left:=0
     right:=0
     maxLength:=0
     for right<len(s){
-        freq[string(s[right])]+=1
-        for freq[string(s[right])]>2{
-            freq[string(s[left])]--
+        freq[s[right]]++
+        for freq[s[right]]>2{
+            freq[s[left]]--
             left++
         }
         currentLength:=right-left+1
-        if currentLength>=maxLength{
+        if currentLength>maxLength{
             maxLength=currentLength
         }
         right++
